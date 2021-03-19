24c77ad261e78983edf9b84194fe35f0771df329
CaioKeidi <caiokeidi@gmail.com>
2021-03-19 20:54:09 -0300

    feat: models Professor in app Escola

--- escoladanca/curso/models.py
+++ escoladanca/curso/models.py
@@ -3,12 +3,12 @@ from django.db import models
 class Curso(models.Model):
     nome = models.CharField(max_length=50, blank=False)
     niveis = models.CharField(max_length=100, blank=False)
-    descricao = models.TextField(max_length=300, default='')
+    descricao = models.TextField(max_length=1000, default='')
     ativo = models.BooleanField(default=True)
 
     def __str__(self):
         return self.nome
 
 class ImagemCurso(models.Model):
-    imoveis = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     foto = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
--- escoladanca/escola/models.py
+++ escoladanca/escola/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Professor(models.Model):
+    nome = models.CharField(max_length=100)
+    apelido = models.CharField(max_length=30)
+    descricao = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.nome
+    
+class ImagemProfessor(models.Model):
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
+
+
+
 
