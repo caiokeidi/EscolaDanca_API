021ab53756b6845235ffd1687470945c898e7ae0
CaioKeidi <caiokeidi@gmail.com>
2021-03-19 15:55:40 -0300

    feat: new curso Model Curso and ImagemCurso

--- escoladanca/curso/models.py
+++ escoladanca/curso/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    nome = models.CharField(max_length=50, blank=False)
+    niveis = models.CharField(max_length=100, blank=False)
+    descricao = models.CharField(max_length=300, default='')
+    ativo = models.BooleanField(default=True)
+
+class ImagemCurso(models.Model):
+    imoveis = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
