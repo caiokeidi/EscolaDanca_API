0e3a4668b797eebcb023bbd6689fae7ec53d850a
CaioKeidi <caiokeidi@gmail.com>
2021-03-19 21:08:11 -0300

    feat: admin config on app Escola

--- escoladanca/escola/admin.py
+++ escoladanca/escola/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from escola.models import Professor
 
-# Register your models here.
+class ProfessoresAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'apelido')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'apelido',)
+
+admin.site.register(Professor, ProfessoresAdmin)
